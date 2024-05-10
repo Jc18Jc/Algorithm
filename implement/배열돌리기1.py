04d75c51c3c3330f168adf7fd81e6bd66480a96f
JC18JC <jc5467@naver.com>
2024-05-11 08:54:02 +0900

    5.11 Algorithm

--- implement/배열돌리기1.py
+++ implement/배열돌리기1.py
@@ -1,18 +1,28 @@
 N,M,R = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-board2 = [[-1 for _ in range(M)] for _ in range(N)]
 for i in range(min(N,M)//2):
     r=R%((N-i*2)*2+(M-i*2)*2-4)
+    tmp = board[i][i:M-i]
+    for j in range(i+1, N-i):
+        tmp.append(board[j][M-i-1])
+    for j in range(M-2-i, i-1, -1):
+        tmp.append(board[N-i-1][j])
+    for j in range(N-i-2, i, -1):
+        tmp.append(board[j][i])
+    tmp = tmp[r:]+tmp[:r]
+    k=0
     for j in range(i, M-i):
-        if r-j+i < 0:
-            board2[i][j-r]=board[i][j]
-            board2[N-i-1][M-j-1+r]=board[N-i-1][M-j-1]
-            continue
-        if r-j+i < N-i:
-            board2[i][j-r]=board[r-j+2*i-N][i]
-            board2[N-i-1][M-j-1+r]=board[N-1+r-j][M-i-1]
-            continue
-        if r-j-N+3*i < M-i:
-            board2[i][j-r]=board[r-j-N+4*i-M][i]
-            board2[N-i-1][M-j-1+r]=board[i][M-i-1]
-            continue
+        board[i][j]=tmp[k]
+        k+=1
+    for j in range(i+1, N-i):
+        board[j][M-1-i]=tmp[k]
+        k+=1
+    for j in range(M-2-i, i-1, -1):
+        board[N-1-i][j]=tmp[k]
+        k+=1
+    for j in range(N-2-i, i, -1):
+        board[j][i]=tmp[k]
+        k+=1
+
+for item in board:
+    print(*item)
