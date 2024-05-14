dfbe5fb7704b682328a2f910613f95029c3bfd37
JC18JC <jc5467@naver.com>
2024-05-14 23:01:56 +0900

    5.14 Algorithm

--- implement/컨베이어벨트위의로봇.py
+++ implement/컨베이어벨트위의로봇.py
@@ -1,22 +1,29 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 belt = [False for _ in range(N)]
-b = answer = 0
+b, answer = 0, 0
 while b < K:
     answer+=1
-    if belt[-(answer%N)]:
-        belt[-(answer%N)]=False
     for i in range(N-2, -1, -1):
-        if belt[i-(answer%N)] and not belt[i-(answer%N)+1] and A[i-(answer%(2*N))+1]:
-            A[i-(answer%(2*N))+1]-=1
-            if not A[i-(answer%(2*N))+1]:
+        if belt[i]:
+            belt[i+1]=True
+            belt[i]=False
+    if belt[-1]:
+        belt[-1]=False
+    d=-(answer%(2*N))
+    for i in range(N-2, -1, -1):
+        if belt[i] and not belt[i+1] and A[i+1+d]:
+            A[i+1+d]-=1
+            if not A[i+1+d]:
                 b+=1
-            belt[i-(answer%N)+1]=True
-            belt[i-(answer%N)]=False
-    if A[-(answer%(2*N))]:
-        A[-(answer%(2*N))]-=1
-        belt[-(answer%N)]=True
-        if not A[-(answer%(2*N))]:
+            belt[i+1]=True
+            belt[i]=False
+    if belt[-1]:
+        belt[-1]=False
+    if A[d] and not belt[0]:
+        A[d]-=1
+        belt[0]=True
+        if not A[d]:
             b+=1
 
 print(answer)
