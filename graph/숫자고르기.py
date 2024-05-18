3a755f7e5d80ab67987fb37ab62b73f6a5ec1331
JC18JC <jc5467@naver.com>
2024-05-18 09:03:59 +0900

    5.18 Algorithm

--- graph/숫자고르기.py
+++ graph/숫자고르기.py
@@ -1,17 +1,25 @@
-def dfs(v,i):
-    visited[v]=True
-    w=array[v]
-    if not visited[w]:
-        dfs(w,i)
-    elif visited[w] and w==i:
-        result.append(w)
 N=int(input())
+
+def dfs(start,s):
+    e=array[s]
+    if visited[e]:
+        return False
+    visited[e]=True
+    if e==start:
+        answer.append(s)
+        return True
+    if dfs(start, array[s]):
+        answer.append(s)
+        return True
+    return False
+
 array=[0]+[int(input()) for _ in range(N)]
-result=[]
+answer=[]
 for i in range(1,N+1):
-    visited=[False]*(N+1)
-    dfs(i,i)
-print(len(result))
-result.sort()
-for i in result:
+    visited=[False for _ in range(N+1)]
+    dfs(i,array[i])
+answer=list(set(answer))
+print(len(answer))
+answer.sort()
+for i in answer:
     print(i)
