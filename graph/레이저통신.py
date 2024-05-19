34a53218aef2dd0585df845741559331fb7d6137
JC18JC <jc5467@naver.com>
2024-05-19 16:22:03 +0900

    5.19 Algorithm

--- graph/레이저통신.py
+++ graph/레이저통신.py
@@ -1,63 +1,40 @@
-from queue import PriorityQueue
+from collections import deque
 delta = [[1, -1, 0, 0], [0, 0, 1, -1]]
-dir = {'D':0, 'U':1, 'R':2, 'L':3}
 M, N = map(int, input().split())
 board = list(list(input()) for _ in range(N))
+ci = []
+cj = []
 for i in range(N):
     for j in range(M):
         if board[i][j]=='C':
-            si, sj = i, j
-pq = PriorityQueue()
+            ci.append(i)
+            cj.append(j)
+visited = [[[10000 for _ in range(4)] for _ in range(M)] for _ in range(N)]
+d = deque()
 for k in range(4):
-    i, j = si+delta[0][k], sj+delta[1][k]
-    if -1 < i < N and -1 < j < M:
-        if board[i][j] == '.':
-            pq.put((0, i, j, k))
-            board[i][j]=0
-        elif board[i][j] == 'C':
+    visited[ci[0]][cj[0]][k]=0
+    i, j = ci[0]+delta[0][k], cj[0]+delta[1][k]
+    if -1 < i < N and -1 < j < M and board[i][j]=='.':
+        if board[i][j]=='.':
+            d.append((i,j,k,0))
+        elif board[i][j]=='C':
             exit(print(0))
-board[si][sj]=-1
-tmp=10000
-while True:
-    c, i, j, d = pq.get()
-    print(i,j)
-    if tmp==c:
-        exit(print(c))
-    if board[i][j] < c:
-        continue
-    nd = [0,1]
-    if d==0 or d==1:
-        nd=[2,3]
-    di, dj = i+delta[0][d], j+delta[1][d]
-    for k in range(2):
-        ti, tj = (i+delta[0][nd[k]], j+delta[1][nd[k]])
-        if -1 < ti < N and -1 < tj < M:
-            if board[ti][tj] == '.':
-                pq.put((c+1, ti, tj, nd[k]))
-                board[ti][tj]=c+1
-            elif board[ti][tj] == 'C':
-                tmp=c+1
+while d:
+    i, j, t, c=d.popleft()
+    di, dj = i, j
+    nd = [0, 1]
+    if t==0 or t==1: nd=[2, 3]
     while -1 < di < N and -1 < dj < M:
-        if board[di][dj]=='.':
-            pq.put((c, di, dj, d))
-            board[di][dj]=c
-            for k in range(2):
-                ti, tj = (di+delta[0][nd[k]], dj+delta[1][nd[k]])
-                if -1 < ti < N and -1 < tj < M:
-                    if board[ti][tj] == '.':
-                        pq.put((c+1, ti, tj, nd[k]))
-                        board[ti][tj]=c+1
-                    elif board[ti][tj] == 'C':
-                        for item in board:
-                            print(*item)
-                        tmp=c+1
-        elif board[di][dj]=='C':
-            exit(print(c))
-        elif board[di][dj]!='*':
-            if board[di][dj] > c:
-                pq.put((c, di, dj, d))
-                board[di][dj]=c
-        else:
+        if visited[di][dj][t] <= c or board[di][dj]=='*':
             break
-        di+=delta[0][d]
-        dj+=delta[1][d]
+        visited[di][dj][t]=c
+        for k in range(2):
+            dir=nd[k]
+            ti, tj = di+delta[0][dir], dj+delta[1][dir]
+            if -1 < ti < N and -1 < tj < M:
+                if (board[ti][tj]=='.' or board[ti][tj]=='C') and visited[ti][tj][t] > c+1:
+                    d.append((ti, tj, dir, c+1))
+        di+=delta[0][t]
+        dj+=delta[1][t]
+
+print(min(visited[ci[1]][cj[1]]))
