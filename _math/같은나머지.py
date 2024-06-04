b1d77eccbf22c06815a8ecc2cb1bc3bde8734b05
JC18JC <jc5467@naver.com>
2024-06-04 23:37:49 +0900

    6.4 Algorithm

---
+++ _math/같은나머지.py
@@ -0,0 +1,15 @@
+import sys
+from math import gcd
+
+n = int(sys.stdin.readline())
+
+num = list(map(int, sys.stdin.readline().split()))
+
+num.sort()
+
+minus = []
+
+for i in range(n - 1):
+    minus.append(num[i + 1] - num[i])
+    
+print(gcd(*minus))
